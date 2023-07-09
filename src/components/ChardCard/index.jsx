a0cd7a8b8e502f6707cf15ef34090dfaa286c943
educcarnn <educcarnn@gmail.com>
2023-07-09 12:04:07 -0300

    feat: add carrousel responsive

--- src/components/ChardCard/index.jsx
+++ src/components/ChardCard/index.jsx
@@ -1,14 +1,21 @@
 import { useCharacterContext } from "./../../contexts/RickAndMortyContext";
 import ParticleEffect from "../Particles";
 import "./style.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+import { useMediaQuery } from "react-responsive";
 
 function ChardCard() {
   const { characters, currentPage, totalPages, nextPage, previousPage } =
-    useCharacterContext();
+    useCharacterContext()
+    
+   
+
+  const shouldShowCarousel = useMediaQuery({ maxWidth: 800 });
 
   const itemCard = ({ id, name, status, image }) => {
     return (
-      <div key={id}>
+      <div key={id} className="carousel-image">
         {status === "Alive" ? (
           <li className="li-Alive">
             <span className="text-card">{name}</span>
@@ -28,7 +35,13 @@ function ChardCard() {
     <>
       <div className="content">
         <h1 className="title">Rick and Morty</h1>
-        <ul className="Ul-Father">{characters?.map(itemCard)}</ul>
+        {shouldShowCarousel ? (
+          <Carousel>
+            {characters?.map(itemCard)}
+          </Carousel>
+        ) : (
+          <ul className="Ul-Father">{characters?.map(itemCard)}</ul>
+        )}
         <div>
           <button onClick={previousPage} disabled={currentPage === 1}>
             Anterior
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,4 +1,5 @@
-export default function Home(){
+export default function Routes(){
+
     return(
         <div>
             
